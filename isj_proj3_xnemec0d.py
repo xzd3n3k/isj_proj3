ccbd5aa03395ed5dc909ccaff65907dff56a4673
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-15 20:18:28 +0100

    init

---
+++ isj_proj3_xnemec0d.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# ukol za 2 body
+def first_odd_or_even(numbers):
+    """Returns 0 if there is the same number of even numbers and odd numbers
+       in the input list of ints, or there are only odd or only even numbers.
+       Returns the first odd number in the input list if the list has more even
+       numbers.
+       Returns the first even number in the input list if the list has more odd 
+       numbers.
+
+    >>> first_odd_or_even([2,4,2,3,6])
+    3
+    >>> first_odd_or_even([3,5,4])
+    4
+    >>> first_odd_or_even([2,4,3,5])
+    0
+    >>> first_odd_or_even([2,4])
+    0
+    >>> first_odd_or_even([3])
+    0
+    """
+    even_count = 0
+    odd_count = 0
+
+    for num in numbers:
+        if num % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+
+    if even_count == odd_count or even_count == 0 or odd_count == 0:
+        return 0
+
+    elif odd_count < even_count:
+        for num in numbers:
+            if (num % 2) != 0:
+                return num
+
+    for num in numbers:
+        if (num % 2) == 0:
+            return num
+
+
+# ukol za 3 body
+def to_pilot_alpha(word):
+    """Returns a list of pilot alpha codes corresponding to the input word
+
+    >>> to_pilot_alpha('Smrz')
+    ['Sierra', 'Mike', 'Romeo', 'Zulu']
+    """
+
+    pilot_alpha = ['Alfa', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot',
+        'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima', 'Mike',
+        'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango',
+        'Uniform', 'Victor', 'Whiskey', 'Xray', 'Yankee', 'Zulu']
+
+    pilot_alpha_list = []
+
+    for char in word:
+        for alpha in pilot_alpha:
+            if alpha[0].lower() == char.lower():
+                pilot_alpha_list.append(alpha)
+                break
+
+    return pilot_alpha_list
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
